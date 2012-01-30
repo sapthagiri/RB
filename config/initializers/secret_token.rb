c86744233af24e8412ca664498c77d6248e9bede
sapthagiri <sapthagiri269@gmail.com>
2012-01-30 08:20:02 +0530

    Intitial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBay::Application.config.secret_token = '3957d2b0e27016a88ab9da656e1dce42cc2cc1985c9760580788f496a41ea15ae0a93df87db765c62645d1b1f75bf5bbd7525b0b6e1328ba7eb5e2fdf78a7cd1'
